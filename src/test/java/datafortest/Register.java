0607a07e17fc30e496378ae797bc7e77bc29dc8f
Ekaterina Antonova <kate.antonova1209@gmail.com>
2022-09-09 15:02:43 +0200

    move pageobject to main/java

--- src/test/java/datafortest/Register.java
+++ src/test/java/datafortest/Register.java
@@ -2,7 +2,7 @@ package datafortest;
 
 import java.util.UUID;
 
-public class Register {
+public abstract class Register {
     public static final String NAME = "Ivan";
     public static final String PASSWORD = "test_password";
     public static final String INVALID_PASSWORD = "12345";
